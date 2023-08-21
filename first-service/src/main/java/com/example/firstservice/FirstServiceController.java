bcf9c4f76ccc0d96f00e8aefbd822cb388b37a28
leewonsuk <tjdjdjrdl123@naver.com>
2023-08-21 22:12:07 +0900

    api gateway service 직접진행 프로젝트

---
+++ first-service/src/main/java/com/example/firstservice/FirstServiceController.java
@@ -0,0 +1,41 @@
+package com.example.firstservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping("/first-service")
+@Slf4j
+public class FirstServiceController {
+    /*Environment env;
+
+    @Autowired
+    public FirstServiceController(Environment env) {
+        this.env = env;
+    }
+*/
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the First service.";
+    }
+
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info(header);
+        return "Hello World in First Service";
+    }
+}
